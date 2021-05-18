5f3ca0b2a97e70161787004d31aca4bd86d44dcd
Pedro <pemoraes1@gmail.com>
2021-05-18 13:09:39 -0300

    Inclui package para configuração do CORS e adiciona rota para tratar rotas inexistentes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const config = require('./config');
 
@@ -7,6 +8,21 @@ const router = express.Router();
 // Carrega as rotas
 const indexRoutes = require('./routes/index-routes');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', indexRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).json({url: req.url, error: 'Caminho não encontrado'}).send();
+});
 
 module.exports = app;
