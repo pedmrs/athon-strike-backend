17f081e131a6685347223765460b93bd7adb47ed
Pedro <pemoraes1@gmail.com>
2021-05-21 12:48:39 -0300

    Melhora retorno das queries que buscam partidas
    Inclui o campo partida_id ao order by da query que retorna todas as partidas a fim de ordenar melhor o resultado. Inclui o campo headshot na seleção da query que retorna informações de uma partida específica

--- src/repositories/partida-repository.js
+++ src/repositories/partida-repository.js
@@ -1,12 +1,12 @@
 const mysql = require('../database/mysql');
 
 exports.getAll = async () => {
-    const result = await mysql.execute(`select p.partida_id, e.nome as 'equipe', j.nickname
+    const result = await mysql.execute(`select p.partida_id, e.nome as equipe, j.nickname
                                         from Partida p
                                         inner join PartidaJogador pj on pj.partida_id = p.partida_id
                                         inner join Jogador j on j.jogador_id = pj.jogador_id
                                         inner join Equipe e on e.equipe_id = pj.equipe_id
-                                        order by e.nome, j.nickname`);
+                                        order by p.partida_id, e.nome, j.nickname`);
 
     return result;
 };
@@ -16,7 +16,7 @@ exports.getById = async (id) => {
                                             when p.equipeVencedora_id then 'Vencedor'
                                             when p.equipePerdedora_id then 'Perdedor'
                                             else 'Erro' end resultado,
-                                            e.nome as equipe, j.nickname, pj.abates, pj.mortes
+                                            e.nome as equipe, j.nickname, pj.abates, pj.mortes, pj.headshots
                                         from PartidaJogador pj
                                         inner join Equipe e on e.equipe_id = pj.equipe_id 
                                         inner join Partida p on p.partida_id = pj.partida_id
