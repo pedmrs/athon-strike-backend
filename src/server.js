b660301e766a901e2570fdf82268b969848146de
Pedro <pemoraes1@gmail.com>
2021-05-21 00:25:09 -0300

    Corrige typo na variável de ambiente utilizada para setar a porta da aplicação

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const debug = require('debug')('nodestr:server');
 const http = require('http');
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
